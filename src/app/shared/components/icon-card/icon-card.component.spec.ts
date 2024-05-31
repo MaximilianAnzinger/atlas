2c9a13021f9686ff0893cdf344f7520a3e2c7319
Maximilian Anzinger <anzinger@in.tum.de>
2024-05-31 13:36:05 +0200

    fix code style

--- src/app/shared/components/icon-card/icon-card.component.spec.ts
+++ src/app/shared/components/icon-card/icon-card.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IconCardComponent } from './icon-card.component';
 
 describe('IconCardComponent', () => {
-  let component: IconCardComponent;
-  let fixture: ComponentFixture<IconCardComponent>;
+    let component: IconCardComponent;
+    let fixture: ComponentFixture<IconCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [IconCardComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(IconCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [IconCardComponent],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(IconCardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
