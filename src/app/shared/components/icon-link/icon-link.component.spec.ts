bfae92ab60ccea4056e19b2cbadcecac9ba69c84
Maximilian Anzinger <maximilian.anzinger@tum.de>
2024-08-04 11:39:58 +0200

    fix style

--- src/app/shared/components/icon-link/icon-link.component.spec.ts
+++ src/app/shared/components/icon-link/icon-link.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IconLinkComponent } from './icon-link.component';
 
 describe('IconLinkComponent', () => {
-  let component: IconLinkComponent;
-  let fixture: ComponentFixture<IconLinkComponent>;
+    let component: IconLinkComponent;
+    let fixture: ComponentFixture<IconLinkComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [IconLinkComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [IconLinkComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(IconLinkComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(IconLinkComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
